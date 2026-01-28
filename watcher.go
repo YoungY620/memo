328f85ac21b8e583e9ff3c44f19131cb5c54ba2d
patrick <youngyee620@qq.com>
2026-01-28 19:18:33 +0800

    fix: remove unused 'root' field from Watcher struct
    The 'root' field was declared as int but never assigned or used.
    The actual root path is stored in 'rootPath' string field.

--- watcher.go
+++ watcher.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Watcher struct {
-	root, debounceMs, maxWaitMs int
+	debounceMs, maxWaitMs int
 	ignorePatterns              []string
 	onChange                    func([]string)
 	watcher                     *fsnotify.Watcher
